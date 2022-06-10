f636c8ed3f63b407f7c1034d25fe2eb2a28248bc
Braga <pimentabraga2@gmail.com>
2022-06-10 20:22:15 -0300

    Carregamento dos dados a partir dos arquivos gerados

--- application/src/App.java
+++ application/src/App.java
@@ -9,11 +9,6 @@ import java.io.IOException;
 
 public class App {
 
-    // TODO do projeto
-    // 1. Verificar porque os métodos não estão contabilizando as comparações
-    // 2. Executar todos os métodos de forma automatica
-    //  2.1. Ler dados dos arquivos gerados
-
     static long trocasTotais;
     static long comparacoesTotais;
 
@@ -23,67 +18,79 @@ public class App {
 
         Utils.gerarDados();
 
-        // TEMPORÁRIO
-        int[] dados = {58,9,42,23,29,33,1,44,47,14,53,25,2,98};
+        int test = Utils.chooseTest();
 
+        int[] dados = Utils.carregarDados(test);
         trocasTotais = 0;
         comparacoesTotais = 0;
-        long []totais={trocasTotais,comparacoesTotais};
+        long[] totais;
 
+        System.out.println("####################################################################");
+
+        System.out.println("HeapSort");
         Utils.askShouldPrint(Constants.QUESTION_DISORDERED_VETOR, dados);
-
         inicio = System.nanoTime();
         totais = Heapsort.heapSort(dados);
-        //Insercao.insertionSort(dados);
-        //Quicksort.quickSort(dados, 0, dados.length-1);
         fim = System.nanoTime();
         tempo = (fim-inicio) * Constants.NANO;
-        System.out.println("HeapSort");
         System.out.printf("Tempo decorrido: %.8f segundos\n", tempo);
         System.out.println("Trocas: " + totais[0]);
         System.out.println("Comparações: " + totais[1]);
         System.out.println();
+        Utils.askShouldPrint(Constants.QUESTION_ORDERED_VETOR, dados);
 
+        System.out.println("####################################################################");
+
+        System.out.println("QuickSort");
+        dados = Utils.carregarDados(test);
+        Utils.askShouldPrint(Constants.QUESTION_DISORDERED_VETOR, dados);
         trocasTotais = 0;
-        comparacoesTotais=0;
+        comparacoesTotais = 0;
         inicio = System.nanoTime();
         totais = Quicksort.quickSort(dados, 0, dados.length-1);
         fim = System.nanoTime();
         tempo = (fim-inicio) * Constants.NANO;
-        System.out.println("QuickSort");
         System.out.printf("Tempo decorrido: %.8f segundos\n", tempo);
         System.out.println("Trocas: " + totais[0]);
         System.out.println("Comparações: " + totais[1]);
         System.out.println();
-    
+        Utils.askShouldPrint(Constants.QUESTION_ORDERED_VETOR, dados);
 
+        System.out.println("####################################################################");
+
+        System.out.println("Selection");
+        dados = Utils.carregarDados(test);
+        Utils.askShouldPrint(Constants.QUESTION_DISORDERED_VETOR, dados);
         trocasTotais = 0;
         comparacoesTotais=0;
         inicio = System.nanoTime();
         totais = Selecao.selectionSort(dados);
         fim = System.nanoTime();
         tempo = (fim-inicio) * Constants.NANO;
-        System.out.println("Selection");
         System.out.printf("Tempo decorrido: %.8f segundos\n", tempo);
         System.out.println("Trocas: " + totais[0]);
         System.out.println("Comparações: " + totais[1]);
         System.out.println();
+        Utils.askShouldPrint(Constants.QUESTION_ORDERED_VETOR, dados);
 
+        System.out.println("####################################################################");
+
+        System.out.println("Insertion");
+        dados = Utils.carregarDados(test);
+        Utils.askShouldPrint(Constants.QUESTION_DISORDERED_VETOR, dados);
         trocasTotais = 0;
         comparacoesTotais=0;
         inicio = System.nanoTime();
         totais = Insercao.insertionSort(dados);
         fim = System.nanoTime();
         tempo = (fim-inicio) * Constants.NANO;
-        System.out.println("Insertion");
         System.out.printf("Tempo decorrido: %.8f segundos\n", tempo);
         System.out.println("Trocas: " + totais[0]);
         System.out.println("Comparações: " + totais[1]);
         System.out.println();
+        Utils.askShouldPrint(Constants.QUESTION_ORDERED_VETOR, dados);
 
-        
+        System.out.println("####################################################################");
     }
-
-    
 
 }
--- application/src/constants/Constants.java
+++ application/src/constants/Constants.java
@@ -3,7 +3,7 @@ package constants;
 public class Constants {
 
     public static final double NANO = (1.0f/1_000_000_000); // Nanossegundo: 1/10^9 segundos (para marcação de tempo)
-    public static final int[] TAMANHOS = {10, 100};
+    public static final int[] TAMANHOS = {1000, 10_000, 100_000, 1_000_000};
     public static final String QUESTION_DISORDERED_VETOR = "Deseja imprimir o vetor desordenado? (y ou n): ";
     public static final String QUESTION_ORDERED_VETOR = "Deseja imprimir o vetor ordenado? (y ou n): ";
 
